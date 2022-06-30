a432629bd16037b19c4ec0baec4658b5d0b39de7
Michael Linares <47120034+mlinares4582@users.noreply.github.com>
2022-06-30 18:30:37 -0400

    Add files via upload

---
+++ functions/naruto-api-lambda-handler.js
@@ -0,0 +1,33 @@
+//Create clients and set shared const values outside of the handler.
+//Get the DynamoDB table name from environment variables.
+const tableName = process.env.TABLE_NAME;
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk.clients.dynamodb');
+const documentClient = new dynamodb.DocumentClient();
+
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`Only GET requests are accepted: ${event.httpMthod}`);
+    }
+
+    console.info('recieved:', event);
+
+    var params = {
+        TableName: tableName,
+    };
+
+    const data = await documentClient.scan(params).promise();
+    const items = data.Items;
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    };
+
+    // All log statements are written to CloudWatch Logs
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    console.info(`Deployment Test 1.2`)
+    return response;
+}
